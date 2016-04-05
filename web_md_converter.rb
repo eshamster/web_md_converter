1b106efc09fdc5d71420ffc94b793d723357c0fa
eshamster <hamgoostar@gmail.com>
2016-04-05 23:05:01 +0900

    Refactor javascript

--- web_md_converter.rb
+++ web_md_converter.rb
@@ -32,16 +32,20 @@ __END__
 %html
   %head
     :javascript
+      function foreach_selector(selector, func) {
+        var elems = document.querySelectorAll(selector);
+        Array.prototype.forEach.call(elems, function(elem) {
+          func(elem);
+        });
+      }
       function hide_all_conf () {
-        var confs = document.querySelectorAll('[class*="conf"]');
-        Array.prototype.forEach.call(confs, function(conf) {
+        foreach_selector('[class*="conf"]', function(conf) {
           conf.style.display = 'none';
         });
       }
-      function show_one_conf_only (selector) {
+      function show_one_conf_only (elem) {
         hide_all_conf();
-        var confs = document.querySelectorAll('.' + selector.value + '_conf');
-        Array.prototype.forEach.call(confs, function(conf) {
+        foreach_selector('.' + elem.value + '_conf', function(conf) {
           conf.style.display = '';
         });
       }
