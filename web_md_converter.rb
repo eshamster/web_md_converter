b3f3c0803276f612e10ad12ab94cd5930bcab479
eshamster <hamgoostar@gmail.com>
2016-04-05 22:57:06 +0900

    Implement a sample of a configuration changer

--- web_md_converter.rb
+++ web_md_converter.rb
@@ -30,11 +30,28 @@ __END__
 
 @@index
 %html
+  %head
+    :javascript
+      function hide_all_conf () {
+        var confs = document.querySelectorAll('[class*="conf"]');
+        Array.prototype.forEach.call(confs, function(conf) {
+          conf.style.display = 'none';
+        });
+      }
+      function show_one_conf_only (selector) {
+        hide_all_conf();
+        var confs = document.querySelectorAll('.' + selector.value + '_conf');
+        Array.prototype.forEach.call(confs, function(conf) {
+          conf.style.display = '';
+        });
+      }
   %body
     %form{:action => '/upload', :method => 'POST', :enctype => 'multipart/form-data'}
       %input{:type => 'file', :name => 'file'}
-      %select{:name => 'output_type'}
+      %select{:name => 'output_type', :onChange => 'show_one_conf_only(this);'}
         %option{:value => 'html'} html
         %option{:value => 'docx'} word
       %input{:type => 'submit', :value => 'upload'}
       %input{:type => 'hidden', :name => '_method', :value => 'put'}
+    %div{:class => 'html_conf'} configuration of html
+    %div{:class => 'docx_conf', :style => 'display:none'} configuration of docx
