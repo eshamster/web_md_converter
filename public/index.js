61d621a50e5d560b68b59ae776667e03f8e365fd
eshamster <hamgoostar@gmail.com>
2016-05-01 00:07:50 +0900

    Modulalize js functions for template

--- public/index.js
+++ public/index.js
@@ -22,69 +22,73 @@ function show_one_conf_only (target_type) {
 
 function do_after_switch_type (after_type) {
     show_one_conf_only(after_type);
-    update_template_selector(after_type);
+    template.update_template_selector(after_type);
 }
 
-var templates_list = null; // as json
+var template =
+    (function() {
+        var templates_list = null; // as json
 
-function append_template_option(selector, name) {
-    var option = document.createElement('option');
-    option.value = name;
-    option.textContent = name;
-    selector.appendChild(option);
-}
+        function append_template_option(selector, name) {
+            var option = document.createElement('option');
+            option.value = name;
+            option.textContent = name;
+            selector.appendChild(option);
+        };
 
-function update_template_selector_impl(target) {
-    if (templates_list === null) {
-        return;
-    }
-    var selector = document.querySelector('#template_selector');
-    selector.textContent = null;
-    append_template_option(selector, '');
-    Array.prototype.forEach.call(templates_list[target], function(name) {
-        append_template_option(selector, name);
-    });
-}
+        function update_template_selector_impl(target) {
+            if (templates_list === null) {
+                return;
+            }
+            var selector = document.querySelector('#template_selector');
+            selector.textContent = null;
+            append_template_option(selector, '');
+            Array.prototype.forEach.call(templates_list[target], function(name) {
+                append_template_option(selector, name);
+            });
+        };
 
-// TODO: avoid duplicated GET request when this is called in many times at short intervals
-function update_template_selector(target) {
-    if (templates_list != null) {
-        update_template_selector_impl(target);
-    }
-    else {
-        request
-            .get('/templates/lists')
-            .end(function (err, res) {
-                if (err === null) {
-                    templates_list = JSON.parse(res.text);
+        return {
+            // TODO: avoid duplicated GET request when this is called in many times at short intervals
+            update_template_selector: function(target) {
+                if (templates_list != null) {
                     update_template_selector_impl(target);
                 }
-            });
-    }
-}
-
-function get_template(type, name) {
-    try {
-        var form = document.template_get_form;
-        form.type.value = type;
-        form.name.value = name;
-        form.submit();
-    }
-    finally {
-        // This function is assumed to be called in another form not in template_get_form.
-        // So it is needed to prevent executing submitting in the former form
-        return false;
-    }
-}
-
-function post_template() {
-    try {
-        var form = document.template_post_form;
-        var main_form = document.main_form;
-        form.type.value = main_form.output_type.value;
-        form.name.value = form.file.value.split(/[\/\\]/).pop();
-    }
-    finally {
-        return false;
-    }
-}
+                else {
+                    request
+                        .get('/templates/lists')
+                        .end(function (err, res) {
+                            if (err === null) {
+                                templates_list = JSON.parse(res.text);
+                                update_template_selector_impl(target);
+                            }
+                        });
+                }
+            },
+            get: function(type, name) {
+                try {
+                    var form = document.template_get_form;
+                    form.type.value = type;
+                    form.name.value = name;
+                    form.submit();
+                }
+                finally {
+                    // This function is assumed to be called in another form
+                    // not in template_get_form. So it is needed to prevent
+                    // executing submitting in the former form
+                    return false;
+                }
+            },
+            post: function() {
+                try {
+                    var form = document.template_post_form;
+                    var main_form = document.main_form;
+                    form.type.value = main_form.output_type.value;
+                    form.name.value = form.file.value.split(/[\/\\]/).pop();
+                }
+                finally {
+                    return false;
+                }
+            }
+        }
+    }());
