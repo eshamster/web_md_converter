9b6269d41808fd607fab4135c8a008a803450e40
eshamster <hamgoostar@gmail.com>
2016-05-02 00:44:36 +0900

    Implement updating the template selector after updating templates

--- public/index.js
+++ public/index.js
@@ -22,12 +22,18 @@ function show_one_conf_only (target_type) {
 
 function do_after_switch_type (after_type) {
     show_one_conf_only(after_type);
-    template.update_template_selector(after_type);
+    template_list.update_template_selector(after_type);
 }
 
-var template_manager =
+function get_current_type() {
+    return document.main_form.output_type.value;
+}
+
+var template_list =
     (function() {
-        var templates_list = null; // as json
+        // TODO: Exclusive updating for this list
+        var list = null; // as JSON
+        var updating_hooks = [];
 
         function append_template_option(selector, name) {
             var option = document.createElement('option');
@@ -37,21 +43,57 @@ var template_manager =
         };
 
         function update_template_selector_impl(target) {
-            if (templates_list === null) {
+            if (list === null) {
                 return;
             }
             var selector = document.querySelector('#template_selector');
             selector.textContent = null;
             append_template_option(selector, '');
-            Array.prototype.forEach.call(templates_list[target], function(name) {
+            Array.prototype.forEach.call(list[target], function(name) {
                 append_template_option(selector, name);
             });
         };
 
+        function execute_updating_hooks() {
+            for(var key in updating_hooks) {
+                updating_hooks[key]();
+            }
+        };
+
         return {
-            // TODO: avoid duplicated GET request when this is called in many times at short intervals
+            add: function(type, name) {
+                if (list[type]) {
+                    if (list[type].indexOf(name) >= 0) {
+                        throw new Error("The name in the type is already registered: type, name = " +
+                                        type + ", " + name);
+                    }
+                    list[type].push(name);
+                }
+                else {
+                    list[type] = [name];
+                }
+                execute_updating_hooks();
+            },
+            delete: function(type, name) {
+                if (!list[type]) {
+                    throw new Error("The type is not exist: " + type);
+                }
+                var index = list[type].indexOf(name);
+                if (index < 0) {
+                    throw new Error("The name in the type has not been registered: type, name = " +
+                                    type + ", " + name);
+                }
+                delete list[type][index];
+                execute_updating_hooks();
+            },
+            get: function() {
+                return list;
+            },
+            add_updating_hook: function(hook) {
+                updating_hooks.push(hook);
+            },
             update_template_selector: function(target) {
-                if (templates_list != null) {
+                if (list != null) {
                     update_template_selector_impl(target);
                 }
                 else {
@@ -59,12 +101,22 @@ var template_manager =
                         .get('/templates/lists')
                         .end(function (err, res) {
                             if (err === null) {
-                                templates_list = JSON.parse(res.text);
+                                list = JSON.parse(res.text);
                                 update_template_selector_impl(target);
                             }
                         });
                 }
             },
+        }
+    }());
+
+template_list.add_updating_hook(function () {
+    template_list.update_template_selector(get_current_type());
+});
+
+var template_manager =
+    (function() {
+        return {
             get: function(type, name) {
                 try {
                     var form = document.template_get_form;
@@ -91,6 +143,7 @@ var template_manager =
                         .end(function (err, res) {
                             if (err === null) {
                                 var res_json = JSON.parse(res.text);
+                                template_list.add(res_json.type, res_json.name);
                                 alert("Uploaded successfully!!\n" +
                                      "type: " + res_json.type + "\n" +
                                      "name: " + res_json.name);
@@ -119,8 +172,9 @@ var template_manager =
                         .end(function (err, res) {
                             if (err === null) {
                                 var res_json = JSON.parse(res.text);
+                                template_list.delete(res_json.type, res_json.name);
                                 alert("Deleted successfully!!\n" +
-                                      "type: " + res_json['type'] + "\n" +
+                                      "type: " + res_json.type + "\n" +
                                       "name: " + res_json.name);
                             }
                             else {
